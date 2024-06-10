2fc7e4218f29e83ea56c6f0c4d197eda43220463
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-06-10 19:06:32 +0000

    add some styles

--- main.py
+++ main.py
@@ -16,25 +16,26 @@ from nicegui import ui
 url1="https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
 df = pd.read_csv(url1,header=0,names=['Index', 'Height', 'Weight'], usecols = [0,1,2])
 
+#https://www.cdc.gov/nccdphp/dnpao/growthcharts/training/bmiage/page5_2.html#:~:text=Formula%3A%20weight%20(lb)%20%2F,in)%5D2%20x%20703&text=Then%2C%20calculate%20BMI%20by%20dividing,a%20conversion%20factor%20of%20703.
+#Formula: weight (lb) / [height (in)]2 x 703
 df['BMI'] = round(df.Weight / df.Height**2*703,1)
 
 print(df)
-# df.to_excel('f.xlsx')
 
 def update(*, df: pd.DataFrame, r: int, c: int, value):
-    df.iat[r, c] = value
-    newbmi = round(df.iat[r,2] / df.iat[r,1]**2*703,1)
-    df.iat[r,3]=newbmi
-    bmi_dict[r].content=str(newbmi)
-    df.to_excel('f.xlsx')
-    print("DF",df)
-    # ui.notify(f'Set ({r}, {c}) to {value}')
+    df.iat[r, c] = value #обновляем вес и рост в DF
+
+    newbmi = round(df.iat[r,2] / df.iat[r,1]**2*703,1) #вычисляем BMI c обновленными значениями
+    df.iat[r,3]=newbmi #сохраняем обновленный BMI в DF
+
+    bmi_dict[r].content=str(newbmi) #находим в словаре HTML-объект BMI по строке, в к-й обновлялся вес или рост
+                                    #и заменяем его контент
+    df.to_excel('f.xlsx') #обновляем файл
 
 
 ui.button('Загрузить файл Excel', on_click=lambda: ui.download('f.xlsx'))
-ui.html('qwerty').content='123'
 
-bmi_dict={}
+bmi_dict={} #словарь HTML-объектов BMI
 
 # with ui.grid(rows=len(df.index)+1).classes('grid-flow-col mx-8 mt-8'):
 #     for c, col in enumerate(df.columns):
@@ -56,18 +57,12 @@ with ui.grid(rows=len(df.index)+1).classes('grid-flow-col mx-8 mt-8'):
         ui.label(col).classes('font-bold')
         for r, row in enumerate(df.loc[:, col]):
             if c==0 :
-                ui.html(f'{df.iloc[r,c]}',tag='div')
+                ui.html(f'{df.iloc[r,c]}',tag='div') #индексы
             elif c==3:
-                bmi =ui.html(f'{df.iloc[r,c]}',tag='div')
-                bmi_dict[r]=bmi
-            elif c == 1 or c == 2:
+                bmi = ui.html(f'{df.iloc[r,c]}',tag='h6').classes('font-semibold text-blue-600') #HTML-объект BMI
+                bmi_dict[r] = bmi #сохраняем в словаре с ключем текущей строки
+            elif c == 1 or c == 2: #рост и вес
                 cls = ui.number
                 cls(value=row,on_change=lambda event, r=r, c=c: update(df=df, r=r, c=c, value=event.value))
-      
-        
-
-
-
-
 
 ui.run(uvicorn_logging_level='info')
