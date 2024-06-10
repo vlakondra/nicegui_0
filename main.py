1c74a0b0d06e3d6377f6d4ad68776048f97164ca
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-06-10 05:24:13 +0000

    add bmi

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from pandas.api.types import is_bool_dtype, is_numeric_dtype
 
 from nicegui import ui
 
-df = pd.DataFrame(data={
-    'col1': [x for x in range(4)],
-    'col2': ['This', 'column', 'contains', 'strings.'],
-    'col3': [x / 4 for x in range(4)],
-    'col4': [True, False, True, False],
-})
+# df = pd.DataFrame(data={
+#     'col1': [x for x in range(4)],
+#     'col2': ['This', 'column', 'contains', 'strings.'],
+#     'col3': [x / 4 for x in range(4)],
+#     'col4': [True, False, True, False],
+# })
+
+url1="https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
+df = pd.read_csv(url1,header=0,names=['Index', 'Height', 'Weight'], usecols = [0,1,2])
 
 print(df)
 # df.to_excel('f.xlsx')
