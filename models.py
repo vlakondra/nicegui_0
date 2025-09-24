689aaea64df5388dc27105e16bdc58cf9b37d178
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-09-24 11:02:44 +0500

    second commit

--- models.py
+++
@@ -1,7 +0,0 @@
-from tortoise import fields, models
-
-
-class User(models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-    age = fields.IntField()
--- ursei.py
+++ ursei.py
@@ -2,20 +2,28 @@ from nicegui import app, ui
 
 from database import Student
 
-
 @ui.page("/")
 def index():
-    st1: Student = Student.get()
-    with ui.card(align_items="center"):
-        ui.label("Hello, " + st1.fam + "!")
-        ui.button("Click me!", on_click=lambda: print("Clicked!"))
+    st1: Student = Student.get(Student.id == 12)
+
+    with ui.row().classes('align-center w-screen h-screen items-center justify-center'):
+        with ui.card().style('width: 300px;'):
+            ui.label("Студент: " + st1.fam + "!")
+            ui.button("Click me!", on_click=lambda d: print(st1.fam))
 
 
 @ui.page(path="/students")
 def students():
-    sts = Student.select(Student.id,Student.fam, Student.studname,Student.age, Student.sex, Student.studgroup).limit(5)
-    d = sts.dicts()[0]
-    print(d.keys())
+    sts = (Student.select(
+                Student.id,
+                Student.fam, 
+                Student.studname,
+                Student.age, 
+                Student.sex, 
+                Student.studgroup)
+                .limit(5)
+                )
+
     cols = [
         {"name": "id", "label": "ID", "field": "id", "sortable": False},
         {"name": "fam", "label": "Фамилия", "field": "fam", "sortable": True},
@@ -29,20 +37,18 @@ def students():
             "sortable": True,
         },
     ]
+
     r = [x for x in sts.dicts()]
-    # for z in sts:
-    #     print(z.studname, z.studgroup.name)
-    ui.table(columns=cols,rows=r, row_key="id",pagination=3)
+    with ui.row().classes('align-center w-screen h-screen items-center justify-center'):
+        ui.table(columns=cols, rows=r, row_key="id", pagination=3,title="Студенты")
+
 
 
 @ui.page("/student")
 def student():
     st: Student = Student.get()
     ui.label(text=st.studname + "-"+st.studgroup.name )
-    print(st.fam + "-" + st.studgroup.name)
 
 
 print("Starting server...")
-
-
 ui.run()
