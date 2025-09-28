f55a6229c32aea1b2d89972ab68079e256c80335
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-09-28 23:18:07 +0500

    simple CRUD

--- database.py
+++ database.py
@@ -1,23 +1,26 @@
 from peewee import *
 from peewee import SqliteDatabase
 
-db = SqliteDatabase('ursei.db')
+db = SqliteDatabase("ursei.db")
 
 
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Department(BaseModel):
     name = CharField(unique=True)
 
+
 class Group(BaseModel):
     name = CharField()
-    department = ForeignKeyField(Department, backref='groups')
+    department = ForeignKeyField(Department, backref="groups")
 
-class Student(BaseModel): #переименование полей модели
-    fam = TextField(column_name='surname')
-    studname = TextField(column_name='name')
-    sex = CharField(column_name='gender')  # 'M' или 'F'
+
+class Student(BaseModel):  # переименование полей модели
+    fam = TextField(column_name="surname")
+    studname = TextField(column_name="name")
+    sex = CharField(column_name="gender")  # 'M' или 'F'
     age = IntegerField()
-    studgroup = ForeignKeyField(Group, column_name='group_id', backref='students')
+    studgroup = ForeignKeyField(Group, column_name="group_id", backref="students")
--- ursei.py
+++ ursei.py
@@ -4,16 +4,20 @@ from database import Student
 
 @ui.page("/")
 def index():
-    st1: Student = Student.get(Student.id == 12)
 
-    with ui.row().classes('align-center w-screen h-screen items-center justify-center'):
-        with ui.card().style('width: 300px;'):
-            ui.label("Студент: " + st1.fam + "!")
-            ui.button("Click me!", on_click=lambda d: print(st1.fam))
+    lbl = ui.label(text="Привет, Урсэи!")
+    stt =Student.get()
+    lbl.text = stt.fam
+
+    ui.link('Студенты',target='students')
+    ui.link('Студент',target='student')
 
 
 @ui.page(path="/students")
 def students():
+    #подготовка данных
+    
+    #выполним запрос к бд
     sts = (Student.select(
                 Student.id,
                 Student.fam, 
@@ -24,6 +28,7 @@ def students():
                 .limit(5)
                 )
 
+    #определяем столбцы таблицы
     cols = [
         {"name": "id", "label": "ID", "field": "id", "sortable": False},
         {"name": "fam", "label": "Фамилия", "field": "fam", "sortable": True},
@@ -38,16 +43,120 @@ def students():
         },
     ]
 
-    r = [x for x in sts.dicts()]
-    with ui.row().classes('align-center w-screen h-screen items-center justify-center'):
-        ui.table(columns=cols, rows=r, row_key="id", pagination=3,title="Студенты")
+    studData = [x for x in sts.dicts()]
+    #Подставляем имя группы вместо id
+    for x in studData:
+        x['studgroup'] = Student.get(x['id']).studgroup.name  
+
+    #Данные готовы
+    
+    #Создаем интерфейс пользователя
+    with ui.column().style('margin:auto').classes('align-center  h-screen items-center justify-center'):
+        def updateDialog():
+            if not stdTable.selected:
+                 sendMessage("Выберите студента")
+                 return
+            with ui.dialog() as dialog1, ui.card():
+                with ui.column().classes("h-120 w-80 justify-start"):
+                    faminp=ui.input(label='Фамилия',
+                            value=stdTable.selected[0]['fam'],
+                            validation={'Слишком длинная фамилия': lambda value: len(value) < 10}).style('width:100%')
+                    nameinp=ui.input(label='Имя',
+                            value=stdTable.selected[0]['studname'],
+                            validation={'Слишком длинное имя': lambda value: len(value) < 10}).style('width:100%')
+               
+                    with ui.row().style("width:100%").classes('justify-center'):
+                            ui.button('Отмена', icon='close', on_click=dialog1.close)
+                            ui.button('Сохранить', icon='done',
+                                      on_click=lambda :(
+                                                        updateRow(fam=faminp.value,
+                                                                   name=nameinp.value),
+                                                        dialog1.close(),
+                                                        stdTable.selected.clear(),
+                                                        ))
+
+            dialog1.open()      
+
+
+        def delDialog():
+            '''
+            Диалог подтверждения удаления \n
+            Вызывается при нажатии кнопки Удалить, если выбрана строка
+            '''
+            if not stdTable.selected:
+                 sendMessage("Выберите студента")
+                 return
+            
+            with ui.dialog() as dialog2, ui.card():
+                with ui.column().classes("h-30 justify-between"):
+                    ui.restructured_text(f"""
+                                        Вы действительно хотите удалить студентa
+
+                                        **{stdTable.selected[0]['fam']}** **{stdTable.selected[0]['studname']}**?
+                                        """)
+                    ui.label(" ",)
+
+                    with ui.row().style("width:100%").classes('justify-between'):
+                            ui.button('Отмена', icon='close', on_click=dialog2.close)
+                            ui.button('Удалить', icon='done',
+                                      on_click=lambda :(
+                                                        removeRow(),
+                                                        dialog2.close()
+                                                        ))
+                #Диалог открывается при вызове функции delDialog
+                dialog2.open()
+
+        def updateRow(fam,name):
+             '''Обновление записи в базе'''
+
+             st = Student.get(stdTable.selected[0]['id'])
+
+             st.fam = fam
+             st.studname = name
+             st.save()
+             
+             #обновление строки в таблице
+             #фильтруем по ID источник данных таблицы
+             flt = list(filter(lambda x: x['id'] == st.id, studData))[0]
+             #и обновляем поля в источнике
+             flt['fam'] = fam
+             flt['studname'] = name
+
+             stdTable.update() #обновляем таблицу
+
+        def removeRow():
+            #удаление записи из базы
+            delstd = Student.get(stdTable.selected[0]['id']).delete_instance()
+            #удаление строки из таблицы
+            stdTable.remove_rows(stdTable.selected)
+            sendMessage("Студент удален!")
+
+        #Выводим таблицу с данными  
+        stdTable = ui.table(columns=cols, rows=studData, row_key="id",
+                  pagination=3,title="Студенты",
+                  selection='single')     
+        
+        #Под таблицей - кнопки вызова диалогов изменения и удаления
+        with ui.row().style("width: 100%; justify-content: flex-end;"):
+            ui.button(text="Изменить",on_click=updateDialog )
+            ui.button(text="Удалить", on_click=delDialog)
+
+    def sendMessage(text):
+        """Вывод сообщений в нижней части экрана"""
+        if text:
+            ui.notify(  
+                    text,
+                    type="warning",
+                    timeout=2000,
+                )
+
 
 
 
 @ui.page("/student")
 def student():
     st: Student = Student.get()
-    ui.label(text=st.studname + "-"+st.studgroup.name )
+    ui.label(text= f'{st.fam}  {st.studname}  Группа: {st.studgroup.name}' )
 
 
 print("Starting server...")
