56cd20e4efa2338b0e28f551938ddde179e45af9
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-10-03 01:24:18 +0500

    add departs

--- stud_group.py
+++ stud_group.py
@@ -1,62 +1,96 @@
 from nicegui import app, ui
-from database import Student, Group
+from database import Department, Student, Group
 
-def StudentsByGroup(grp_id):
-    '''Возвращает список студентов по выбранной группе.\n
-    Вызывается при изменении группы в выпадающем списке,\n
-    а также при запуске приложения'''
-
-    std = (Student
-       .select(Student.id,
-               Student.fam.alias('Фамилия'),
-               Student.studname.alias('Имя'),
-               Student.age.alias('Возраст'),
-               Student.studgroup)
-       .where(Student.studgroup == grp_id)
-       )
-    #строим список словарей с данными студентов
-    studData = [x for x in std.dicts()]
-    return studData
-
-#центрируем контент
-nc = ui.query('.nicegui-content')
-st = '''background:lightblue;
+# центрируем контент
+nc = ui.query(".nicegui-content")
+st = """background:lightblue;
         width:100%;
         height:100vh;
         align-items:center;
-        justify-content:center'''
+        justify-content:center"""
 
 nc.style(st)
 
-#Получаем список групп
-grp = Group.select(Group.id, Group.name)
+def GroupsByDepartment(dpr_id):
+    """Группы по выбранному факультету"""
+    grp = (Group
+           .select(Group.id, Group.name)
+           .where(Group.department == dpr_id)
+    )
+    #строим словарь с группами
+    gd = {}
+    for g in grp:
+        gd[g.id] = g.name
 
-#Поскольку данные для выпадающего списка - словарь
-#приходится его строить
-gd={}
-for g in grp:
-    gd[g.id]=g.name
+    return gd, list(gd.keys())[0]  # ключ 1-й группы
 
-#выводим выпадающий список с группами
-#группа по умолчанию с индексом 5. M.б. любая
+def StudentsByGroup(grp_id):
+    """Возвращает список студентов по выбранной группе.\n
+    Вызывается при изменении группы в выпадающем списке,\n
+    а также при запуске приложения"""
+
+    std = Student.select(
+        Student.id,
+        Student.fam.alias("Фамилия"),
+        Student.studname.alias("Имя"),
+        Student.age.alias("Возраст"),
+        Student.studgroup,
+    ).where(Student.studgroup == grp_id)
+    # строим список словарей с данными студентов
+    studData = [x for x in std.dicts()]
+    return studData
+
+# Получаем список факультетов
+dpr = Department.select(Department.id, Department.name)
+# словарь для выпадающего списка факультетов
+departs = {}
+for d in dpr:
+    departs[d.id] = d.name
+
+# выводим выпадающиe списки с группами и факультетами
 with ui.card().style("margin:auto"):
-    ui.select(gd, 
-            value=list(gd.keys())[5], 
-            on_change = lambda e:(
+    with ui.row():
+        with ui.column().style("gap:0"):
+            ui.label("Факультет:")
+            # выпадающий список с факультетами
+            dp = ui.select(
+                departs,
+                value=list(departs.keys())[0],
+                on_change=lambda e: (
+                    print("фак-т"),
+                    new_grpdata := GroupsByDepartment(e.value)[0],
+                    st.set_options(new_grpdata),
+                    st.set_value(list(new_grpdata.keys())[0]),
+                ),
+            ).style("height:40px")
+
+        grpdep = GroupsByDepartment(dp.value)
+        ui.label("Группы:")
+        st = ui.select(
+            options=grpdep[0],
+            value=grpdep[1],
+            on_change=lambda e: (
+                print("grp"),
                 new_stdata := StudentsByGroup(e.value),
                 stdTable.rows.clear(),
-                #получаем новые данные, и передаем их в таблицу    
+                # получаем новые данные, и передаем их в таблицу
                 stdTable.rows.extend(new_stdata),
-                stdTable.update()
-                ))
+                stdTable.update(),
+            ),
+        )
 
-    #Выводим таблицу со студентами
-    #содержание таблицы зависит от выбранной группы
-    stdTable = ui.table( rows=StudentsByGroup(list(gd.keys())[5]),
-                    row_key="id",
-                    title="Студенты",
-                    pagination=3,
-                    selection='single')  
+    # Выводим таблицу со студентами
+    # содержание таблицы зависит от выбранной группы
+    stdTable = ui.table(
+        rows=StudentsByGroup(st.value),
+        row_key="id",
+        title="Студенты",
+        pagination=3,
+        selection="single",
+    )
+
+    # ta = ui.textarea("qwerty")
+    # stdTable.bind_filter_from( ta,'value')
 
 
 ui.run()
